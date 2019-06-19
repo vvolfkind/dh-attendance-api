2fca5918af0a83840e86537106989d77c7f6ab32
vvolfkind <rodolfo.raskovsky85@gmail.com>
2019-06-19 10:12:57 -0300

    sgecontroller

---
+++ services/sge.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+const sgeClient = () => {
+    const url = 'https://im.digitalhouse.com';
+    
+    // var options = {
+    //     'method': 'POST',
+    //     'hostname': 'https://im.digitalhouse.com',
+    //     'path': '/api/auth/authorization',
+    //     'headers': {
+    //         'Content-Type': 'application/x-www-form-urlencoded'
+    //     }
+    // };
+}
+
+module.exports = {
+    sgeClient
+}
